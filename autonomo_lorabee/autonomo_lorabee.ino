1a49f5531867ec8bdd46ea19570b29702daeec2d
Gertjan Assies <g.assies@thenewmotion.com>
2016-01-11 08:49:43 +0100

    renamed file and now set charging to false whenever an error occured sending

--- autonomo_lorabee/autonomo_lorabee.ino
+++ autonomo_lorabee/autonomo_lorabee.ino
@@ -95,28 +95,35 @@ void sendMessage(String data) {
     break;
   case NoResponse:
     debugSerial.println("There was no response from the device.");
+    charging = false;
     break;
   case Timeout:
     debugSerial.println("Connection timed-out. Check your serial connection to the device! Sleeping for 20sec.");
     delay(20000);
+    charging = false;
     break;
   case PayloadSizeError:
     debugSerial.println("The size of the payload is greater than allowed. Transmission failed!");
+    charging = false;
     break;
   case InternalError:
     debugSerial.println("Oh No! This shouldn't happen. Something is really wrong! Try restarting the device!\r\nThe program will now halt.");
+    charging = false;
     while (1) {};
     break;
   case Busy:
     debugSerial.println("The device is busy. Sleeping for 10 extra seconds.");
+    charging = false;
     delay(10000);
     break;
   case NetworkFatalError:
     debugSerial.println("There is a non-recoverable error with the network connection. You should re-connect.\r\nThe program will now halt.");
+    charging = false;
     while (1) {};
     break;
   case NotConnected:
     debugSerial.println("The device is not connected to the network. Please connect to the network before attempting to send data.\r\nThe program will now halt.");
+    charging = false;
     while (1) {};
     break;
   case NoAcknowledgment:
